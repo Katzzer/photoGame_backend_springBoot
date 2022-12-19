20dd8f901338eb3611b195c9865fdd52b5ce6246
Pavel Kostal <kostal@quantretail.com>
2022-12-19 19:33:18 +0100

    updated pom.xml, added docker-compose.yml, updated README.MD

--- api/src/main/java/com/pavelkostal/api/apiController/ApiController.java
+++ api/src/main/java/com/pavelkostal/api/apiController/ApiController.java
@@ -8,12 +8,22 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
 @RestController()
 @RequestMapping("api/v1/data")
 @AllArgsConstructor
 public class ApiController {
 
     private final PhotoService photoService;
+    
+    @GetMapping
+    public String hello() {
+        LocalDateTime now = LocalDateTime.now();
+        DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ISO_DATE_TIME;
+        return "Hello from Spring Boot Application Photo Game (" + now.format(dateTimeFormatter) +")";
+    }
 
     @PostMapping()
     public ResponseEntity<Photo> saveImage(@RequestBody Photo photo) {
