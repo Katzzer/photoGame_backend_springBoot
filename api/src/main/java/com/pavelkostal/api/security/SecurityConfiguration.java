2ffd9a5eb895d6d32aa5d33c7dfd150d52f3e127
Pavel Kostal <kostal@quantretail.com>
2022-12-23 17:14:57 +0100

    added open api and Swagger

--- api/src/main/java/com/pavelkostal/api/security/SecurityConfiguration.java
+++ api/src/main/java/com/pavelkostal/api/security/SecurityConfiguration.java
@@ -21,6 +21,7 @@ public class SecurityConfiguration {
 				.cors().and()
 				.csrf().disable()
 				.authorizeHttpRequests((auth) -> auth
+						.requestMatchers("/swagger-ui/**", "/v3/api-docs/**").permitAll()
 						.anyRequest().authenticated()
 						.and()
 				)
