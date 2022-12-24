507bde6e552e5fbc14383e2a6aadfccdd9b0abb1
Pavel Kostal <kostal@quantretail.com>
2022-12-24 11:23:17 +0100

    added error response when JWT is expired or invalid

--- api/src/main/java/com/pavelkostal/api/security/AwsCognitoJwtAuthFilter.java
+++ api/src/main/java/com/pavelkostal/api/security/AwsCognitoJwtAuthFilter.java
@@ -1,17 +1,24 @@
 package com.pavelkostal.api.security;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import jakarta.servlet.*;
 import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 
 @Component
 @Slf4j
-public class AwsCognitoJwtAuthFilter extends GenericFilter {
+public class AwsCognitoJwtAuthFilter extends OncePerRequestFilter {
 
     private final AwsCognitoIdTokenProcessor cognitoIdTokenProcessor;
 
@@ -20,19 +27,26 @@ public class AwsCognitoJwtAuthFilter extends GenericFilter {
     }
 
     @Override
-    public void doFilter(ServletRequest request, ServletResponse response, FilterChain filterChain) throws IOException, ServletException {
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
         Authentication authentication;
-
+    
         try {
-            authentication = this.cognitoIdTokenProcessor.authenticate((HttpServletRequest)request);
+            authentication = this.cognitoIdTokenProcessor.authenticate(request);
             if (authentication != null) {
                 SecurityContextHolder.getContext().setAuthentication(authentication);
             }
-        } catch (Exception var6) {
-            log.error("Cognito ID Token processing error", var6);
+        } catch (Exception exception) {
+            log.error("Cognito ID Token processing error", exception);
             SecurityContextHolder.clearContext();
+            response.setHeader("error", exception.getMessage());
+            response.setStatus(HttpStatus.UNAUTHORIZED.value());
+            Map<String, String> error = new HashMap<>();
+            error.put("error_message", exception.getMessage());
+            response.setContentType(MediaType.APPLICATION_JSON_VALUE);
+            new ObjectMapper().writeValue(response.getOutputStream(), error);
+            response.flushBuffer();
         }
-
+    
         filterChain.doFilter(request, response);
     }
 }
