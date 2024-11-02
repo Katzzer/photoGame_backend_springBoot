17ef473866df42ff16942fd0353e0ac63c5f1910
Katzz Win11 PC <katzz@seznam.cz>
2024-11-02 09:10:13 +0100

    Remove custom CorsFilter bean and fix base path in properties - now backend works fine
    The custom CorsFilter bean has been removed from the SecurityConfiguration class to streamline security configurations. Additionally, corrected the base-controller.path in application.properties by removing the leading slash to ensure URL consistency.

--- api/src/main/java/com/pavelkostal/api/security/SecurityConfiguration.java
+++ api/src/main/java/com/pavelkostal/api/security/SecurityConfiguration.java
@@ -6,9 +6,6 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-import org.springframework.web.filter.CorsFilter;
 
 @Configuration
 @AllArgsConstructor
@@ -27,18 +24,6 @@ public class SecurityConfiguration {
                 .addFilterBefore(awsCognitoJwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
 
         return http.build();
-    }
-
-    @Bean
-    public CorsFilter corsFilter() {
-        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-        CorsConfiguration config = new CorsConfiguration();
-        config.setAllowCredentials(true);
-        config.addAllowedOrigin("http://localhost:8080");
-        config.addAllowedHeader("*");
-        config.addAllowedMethod("*");
-        source.registerCorsConfiguration("/**", config);
-        return new CorsFilter(source);
     }
 
 }
